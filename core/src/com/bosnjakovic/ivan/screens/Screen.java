2626690e326febb9cf9de97c2b83ac3e64040c0e
Ivan <i.bosnjakovic@nisa.com.hr>
2016-03-30 13:50:25 +0200

    Initial commit

---
+++ core/src/com/bosnjakovic/ivan/screens/Screen.java
@@ -0,0 +1,56 @@
+package com.bosnjakovic.ivan.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created by  on 30.3.2016..
+ */
+public class Screen implements com.badlogic.gdx.Screen {
+    public Screen() {
+        Gdx.app.log("Game screen", "Attached");
+    }
+
+    @Override
+    public void render(float delta) {
+        // Sets a Color to Fill the Screen with (RGB = 10, 15, 230), Opacity of 1 (100%)
+        Gdx.gl.glClearColor(10 / 255.0f, 15 / 255.0f, 230 / 255.0f, 1f);
+        // Fills the screen with the selected color
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        Gdx.app.log("GameScreen", "resizing");
+    }
+
+    @Override
+    public void show() {
+        Gdx.app.log("GameScreen", "show called");
+
+    }
+
+    @Override
+    public void hide() {
+        Gdx.app.log("GameScreen", "hide called");
+
+    }
+
+    @Override
+    public void pause() {
+        Gdx.app.log("GameScreen", "pause called");
+
+    }
+
+    @Override
+    public void resume() {
+        Gdx.app.log("GameScreen", "resume called");
+
+    }
+
+    @Override
+    public void dispose() {
+
+
+    }
+}
